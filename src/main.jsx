611513d655b3e742a87d422670d397240d322e3b
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-11-26 23:25:51 +0600

    read data from database by using CRUD - read/get function

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import UpdateCoffee from "./components/UpdateCoffee.jsx";
 import SignUp from "./components/signUp.jsx";
 import SignIn from "./components/signIn.jsx";
 import AuthProvider from "./providers/AuthProvider.jsx";
+import Users from "./components/Users.jsx";
 
 const router = createBrowserRouter([
 	{
@@ -39,6 +40,11 @@ const router = createBrowserRouter([
 			{
 				path: "/signin",
 				element: <SignIn></SignIn>,
+			},
+			{
+				path: "/users",
+				element: <Users></Users>,
+				loader: () => fetch("http://localhost:5000/user"),
 			},
 		],
 	},
